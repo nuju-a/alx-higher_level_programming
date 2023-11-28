de665afe827a3511e5decbd7ed7210b76ec85206
Nuju Azenabor <learnnuju@gmail.com>
2023-11-28 04:12:55 +0100

    requirement done

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import sys
+
+
 def main():
-	sys.stdout = sys.stderr
-	dora  = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-	sys.stderr.write(dora + "\n")
-	sys.exit(1)
+    sys.stdout = sys.stderr
+    dora = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+    sys.stderr.write(dora + "\n")
+    sys.exit(1)
+
+
 main()
