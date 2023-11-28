55870ce3a3927f0e9c358934f001ab81374037cd
Nuju Azenabor <learnnuju@gmail.com>
2023-11-28 04:07:19 +0100

    task100

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+def main():
+	sys.stdout = sys.stderr
+	dora  = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+	sys.stderr.write(dora + "\n")
+	sys.exit(1)
+main()
