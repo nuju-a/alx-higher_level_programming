8092626bb0bd27c101cf619ee11172ffc74f0162
Nuju Azenabor <learnnuju@gmail.com>
2023-12-05 00:21:41 +0100

    task8&7

---
+++ 0x03-python-data_structures/13-is_palindronme.c
@@ -0,0 +1,15 @@
+include "list.h"
+
+/**
+  *is_palindrome - function checks if a singly linked list is apalindrome
+  *@listint_t: singly linked list
+  *@head: pointer to the first element in listint_t
+  *
+  *Return: 0 or 1
+  */
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL)
+		return(1);
+
+}
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,10 +2,10 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple < 2:
-        tuple_a += (0, 0)
-	tuple_b += (0, 0)
-    if tuple > 2:
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
 
-	return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
+    tuple_sum = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return tuple_sum
