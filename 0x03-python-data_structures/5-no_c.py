d41af3357742f17bf8ef08b12bfbca7f04121100
Nuju Azenabor <learnnuju@gmail.com>
2023-12-04 12:55:59 +0100

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@
 def no_c(my_string):
     empty_string = ''
     for i in my_string:
-        if i != 'c' and  i != 'C':
+        if i != 'c' and i != 'C':
             empty_string += i
     return empty_string
