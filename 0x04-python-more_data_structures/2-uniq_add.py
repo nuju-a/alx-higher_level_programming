7c8a3e1e1705737c1ac96cddd958336e301a41ff
Nuju Azenabor <learnnuju@gmail.com>
2023-12-11 03:14:08 +0100

    task2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    new_list = []
+    add = 0
+    for i in my_list:
+        if i not in new_list:
+            add += i
+            new_list.append(i)
+    return(add)
